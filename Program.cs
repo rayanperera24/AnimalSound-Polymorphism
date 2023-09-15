169b2b9aca70d600a6f8b7b437a5463487b049b3
Rayan Perera <rayanp2004@gmail.com>
2023-09-15 12:59:01 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animal_soudn_polymorpishm
+{
+
+    class animal
+    {
+
+        public virtual void animalsound()
+        {
+            Console.WriteLine("animal Makes a Sound");
+        }
+    }
+
+    class pig:animal
+    {
+
+        public override void animalsound()
+        {
+            Console.WriteLine("oink....oink");
+        }
+
+    }
+
+    class dog:animal
+    {
+        public override void animalsound()
+        {
+            Console.WriteLine("woof.....woof");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            animal animal = new animal();
+            animal pig = new pig();
+            animal dog = new dog();
+            pig.animalsound();
+            dog.animalsound();
+
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
